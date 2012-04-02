67af7bfdaeba0ec1e9797794657e01a95853b9a2
dragon2901 <hongty.huynh@gmail.com>
2012-04-02 19:06:20 +0700

    Example to test new project

---
+++ DB2OntoPL/src/dh/protege41/editor/owl/tab/ExampleViewComponent.java
@@ -0,0 +1,30 @@
+package dh.protege41.editor.owl.tab;
+
+import java.awt.BorderLayout;
+
+import org.apache.log4j.Logger;
+import org.protege.editor.owl.ui.view.AbstractOWLViewComponent;
+
+import dh.protege41.owl.Metrics;
+
+
+public class ExampleViewComponent extends AbstractOWLViewComponent {
+    private static final long serialVersionUID = -4515710047558710080L;
+    
+    private static final Logger log = Logger.getLogger(ExampleViewComponent.class);
+    
+    private Metrics metricsComponent;
+
+    @Override
+    protected void disposeOWLView() {
+    }
+
+    @Override
+    protected void initialiseOWLView() throws Exception {
+        setLayout(new BorderLayout());
+        metricsComponent = new Metrics(getOWLModelManager());
+        add(metricsComponent, BorderLayout.CENTER);
+        log.info("Example View Component initialized");
+    }
+
+}
---
+++ DB2OntoPL/src/dh/protege41/editor/owl/tab/MainWorkspaceTab.java
@@ -0,0 +1,7 @@
+package dh.protege41.editor.owl.tab;
+
+import org.protege.editor.owl.ui.OWLWorkspaceViewsTab;
+
+public class MainWorkspaceTab extends OWLWorkspaceViewsTab{
+	
+}
